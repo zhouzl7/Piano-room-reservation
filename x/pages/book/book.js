dcd1eb88f06abee92705064a976b9b62d135c9fe
WandSmith <hasbeenoccupied@126.com>
2018-11-17 01:44:28 +0800

    Add Book Page

---
+++ x/pages/book/book.js
@@ -0,0 +1,287 @@
+// pages/book/book.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    time: [
+      {
+        id:0,
+        timestr: "8:00-9:00",
+        color: "transparent"
+      },
+      {
+        id: 1, timestr: "9:00-10:00", color: "transparent"
+      },
+      {
+        id: 2, timestr: "10:00-11:00", color: "transparent"
+      },
+      {
+        id: 3, timestr: "13:00-14:00", color: "transparent"
+      },
+      {
+        id: 4, timestr: "14:00-15:00", color: "transparent"
+      },
+      {
+        id: 5, timestr: "15:00-16:00", color: "transparent"
+      }
+    ],
+    Days: [
+      {
+        id: "今天",
+        room: [
+          {
+            name: "wtf",
+            disabled: [true, true, false, false, false, false],
+            chosen: [false, false, false, false, false, false],
+            money: 15,
+            multiMoney: 30,
+            color: "transparent"
+          },
+          {
+            name: "test",
+            disabled: [false, false, false, false, false, false],
+            chosen: [false, false, false, false, false, false],
+            money: 15,
+            multiMoney: 30,
+            color: "transparent"
+          }
+        ],
+        color: "transparent"
+      },
+      {
+        id:"明天",
+        room: [
+          {
+            name: "test",
+            disabled: [false, false, false, false, false, false],
+            chosen: [false, false, false, false, false, false],
+            money: 15,
+            multiMoney: 30,
+            color: "transparent"
+          },
+          {
+            name: "wtf",
+            disabled: [false, false, false, false, false, false],
+            chosen: [false, false, false, false, false, false],
+            money: 15,
+            multiMoney: 30,
+            color: "transparent"
+          }
+        ],
+        color: "transparent"
+      },
+      {
+        id: "后天",
+        room: [
+          {
+            name: "wtf",
+            disabled: [false, false, false, false, false, false],
+            chosen: [false, false, false, false, false, false],
+            money: 15,
+            multiMoney: 30,
+            color: "transparent"
+          },
+          {
+            name: "test",
+            disabled: [false, false, false, false, false, false],
+            chosen: [false, false, false, false, false, false],
+            money: 15,
+            multiMoney: 30,
+            color: "transparent"
+          },
+          {
+            name: "test",
+            disabled: [false, false, false, false, false, false],
+            chosen: [false, false, false, false, false, false],
+            money: 15,
+            multiMoney: 30,
+            color: "transparent"
+          }
+        ],
+        color: "transparent"
+      }
+    ],
+    chosenDay: 0,
+    chosenRoom: 0,
+    single: true,
+    money: 0,
+    singleMoney: 0,
+    multiMoney: 0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    //TODO: 刷新当前可用时间.
+    const chosenDay = this.data.chosenDay
+    const chosenRoom = this.data.chosenRoom
+    const length = this.data.Days[chosenDay].room[chosenRoom].disabled.length
+    this.data.Days[chosenDay].color = "#0090CE"
+    this.data.Days[chosenDay].room[chosenRoom].color = "#0090CE"
+    for(let i = 0;i < length;i++){
+      if (this.data.Days[chosenDay].room[chosenRoom].disabled[i]){
+        this.data.time[i].color = "transparent"
+      }else{
+        if (this.data.Days[chosenDay].room[chosenRoom].chosen[i]){
+          this.data.time[i].color = "Yellow"
+        }else{
+          this.data.time[i].color = "transparent"
+        }
+      }
+    }
+    this.setData({
+      time: this.data.time,
+      Days: this.data.Days
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  chooseDay: function(event){
+    let chosenDay = this.data.chosenDay
+    let chosenRoom = this.data.chosenRoom
+    let length = this.data.Days[chosenDay].room[chosenRoom].disabled.length
+    this.data.Days[chosenDay].color = "transparent"
+    this.data.Days[chosenDay].room[chosenRoom].color = "transparent"
+    chosenDay = event.currentTarget.dataset.id
+    this.data.Days[chosenDay].color = "#0090CE"
+    if(this.data.Days[chosenDay].room.length <= chosenRoom){
+      chosenRoom = this.data.Days[chosenDay].room.length - 1
+    }
+    this.data.Days[chosenDay].room[chosenRoom].color = "#0090CE"
+    for (let i = 0; i < length; i++) {
+      if (this.data.Days[chosenDay].room[chosenRoom].disabled[i]) {
+        this.data.time[i].color = "transparent"
+      } else {
+        if (this.data.Days[chosenDay].room[chosenRoom].chosen[i]) {
+          this.data.time[i].color = "Yellow"
+        } else {
+          this.data.time[i].color = "transparent"
+        }
+      }
+    }
+    this.setData({
+      time: this.data.time,
+      Days: this.data.Days,
+      chosenDay: chosenDay,
+      chosenRoom: chosenRoom
+    })
+  },
+
+  chooseRoom: function(event){
+    let chosenDay = this.data.chosenDay
+    let chosenRoom = this.data.chosenRoom
+    let length = this.data.Days[chosenDay].room[chosenRoom].disabled.length
+    this.data.Days[chosenDay].room[chosenRoom].color = "transparent"
+    chosenRoom = event.currentTarget.dataset.id
+    this.data.Days[chosenDay].room[chosenRoom].color = "#0090CE"
+    for (let i = 0; i < length; i++) {
+      if (this.data.Days[chosenDay].room[chosenRoom].disabled[i]) {
+        this.data.time[i].color = "transparent"
+      } else {
+        if (this.data.Days[chosenDay].room[chosenRoom].chosen[i]) {
+          this.data.time[i].color = "Yellow"
+        } else {
+          this.data.time[i].color = "transparent"
+        }
+      }
+    }
+    this.setData({
+      time: this.data.time,
+      Days: this.data.Days,
+      chosenRoom: chosenRoom
+    })
+  },
+
+  chooseTime: function(event) {
+    let index = event.currentTarget.dataset.id
+    let chosenDay = this.data.chosenDay
+    let chosenRoom = this.data.chosenRoom
+    if (this.data.Days[chosenDay].room[chosenRoom].chosen[index] === false){
+      //改变chosen, 增加money, 变色
+      this.data.time[index].color = "yellow"
+      this.data.Days[chosenDay].room[chosenRoom].chosen[index] = true
+      this.data.singleMoney += this.data.Days[chosenDay].room[chosenRoom].money
+      this.data.multiMoney += this.data.Days[chosenDay].room[chosenRoom].multiMoney
+    }else{
+      this.data.time[index].color = "transParent"
+      this.data.Days[chosenDay].room[chosenRoom].chosen[index] = false
+      this.data.singleMoney -= this.data.Days[chosenDay].room[chosenRoom].money
+      this.data.multiMoney -= this.data.Days[chosenDay].room[chosenRoom].multiMoney
+    }
+    this.data.money = this.data.single ? this.data.singleMoney : this.data.multiMoney
+    //this.data.money = this.data.singleMoney
+    this.setData({
+      time: this.data.time,
+      Days: this.data.Days,
+      money: this.data.money,
+      singleMoney: this.data.singleMoney,
+      multiMoney: this.data.multiMoney
+    })
+  },
+
+  singleChange: function (event) {
+    console.log(event)
+    if(event.detail.value){
+      this.setData({
+        money: this.data.singleMoney,
+        single: true
+      })
+    }else{
+      this.setData({
+        money: this.data.multiMoney,
+        single: false
+      })
+    }
+    console.log(this.data.single)
+  }
+})
--- x/pages/mine/mine.js
+++ x/pages/mine/mine.js
@@ -1,28 +1,27 @@
-//index.js
-//获取应用实例
+//mine.js
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    myBook: [],
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
   onLoad: function () {
-    //console.log('wtf')
+    app.globalData.userName = "2016013261 CQ"
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -44,15 +43,12 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  },
-  onShow: function(e){
-    console.log('wtf')
   }
 })
